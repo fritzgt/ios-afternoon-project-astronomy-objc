426d0fdfa6430916c6801ddbafdee39ebc2784c2
exoticproductions <contactexoticproductions@gamail.com>
2021-07-09 22:16:13 +0300

    initial commit

---
+++ Astronomy/Astronomy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by FGT MAC on 7/9/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Astronomy/Astronomy/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Astronomy
+//
+//  Created by FGT MAC on 7/9/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
